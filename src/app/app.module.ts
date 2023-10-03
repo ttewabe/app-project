fff85a84459a0f02e19d07ae0a0950bbb2db1ff4
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-10-02 22:02:19 -0700

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-//import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-//import { SuccessAlertComponent } from './success-alert/success-alert.component';
+// import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+// import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //WarningAlertComponent,
-    //SuccessAlertComponent,
+    // WarningAlertComponent,
+    // SuccessAlertComponent,
     ServerComponent,
     ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    //HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
