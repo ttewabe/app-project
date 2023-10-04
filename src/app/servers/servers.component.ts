751331998104e8c99a39321cb42ce3988b6d2da5
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-10-03 23:11:00 -0700

    added bindable properties and events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus="No serever was created"
+serverName =""
+
   constructor() {
     setTimeout(()=>{
       this.allowNewServer = true;
@@ -18,5 +20,10 @@ export class ServersComponent implements OnInit {
   }
 onCreateSerever(){
 this.serverCreationStatus = 'Server was created'
+}
+
+onUpdateServerName(event:any){
+ 
+  this.serverName = (<HTMLInputElement>event.target).value
 }
 }
